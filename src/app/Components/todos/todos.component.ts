69f2d0b506f573d084dbcd55140e05b091817184
Kali-Venum <subhamdas.abcd@gmail.com>
2023-02-05 22:08:53 +0530

    working on add-todo form.

---
+++ src/app/Components/todos/todos.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Todo } from 'src/app/Models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+})
+export class TodosComponent {
+  todos: Todo[];
+
+  constructor() {
+    this.todos = [
+      {
+        sno: 1,
+        title: 'This is title 1',
+        description: 'This is description 1',
+        isActive: true,
+      },
+      {
+        sno: 2,
+        title: 'This is title 2',
+        description: 'This is description 2',
+        isActive: true,
+      },
+      {
+        sno: 3,
+        title: 'This is title 3',
+        description: 'This is description 3',
+        isActive: true,
+      },
+    ];
+  }
+
+  deleteTodo(todo: Todo) {
+    console.log(todo);
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+  }
+}
